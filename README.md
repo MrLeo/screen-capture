2807052eb5ea2cd710c14e9b9bdd624716e37173
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2021-02-24 18:48:08 +0800

    docs: 地址列表

---
+++ README.md
@@ -0,0 +1,4 @@
+
+- GitHub: https://github.com/MrLeo/screen-capture
+- 浏览器截屏&录像Demo: https://dev.xuebin.me/screen-capture/
+- codepen: https://codepen.io/MrLeo/pen/VwmrJMX
--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 
+// GitHub: https://github.com/MrLeo/screen-capture
 // 浏览器截屏&录像Demo: https://dev.xuebin.me/screen-capture/
 // codepen: https://codepen.io/MrLeo/pen/VwmrJMX
 
